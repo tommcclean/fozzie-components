9a59f068d7284d7200adf0ef92cc88f398328943
Damian Mullins <damian.mullins@just-eat.com>
2020-10-02 16:08:11 +0100

    Empty button component.

---
+++ stories/components/buttons.stories.js
@@ -0,0 +1,14 @@
+export default {
+    title: 'Components/Button',
+
+    parameters: {
+        abstract: {
+            url: 'https://share.goabstract.com/733ca894-a4bb-43e3-a2b1-dd27ff6d00c4'
+        }
+    }
+};
+
+export const MegaModalComponent = () => ({
+});
+
+MegaModalComponent.storyName = 'Example';
